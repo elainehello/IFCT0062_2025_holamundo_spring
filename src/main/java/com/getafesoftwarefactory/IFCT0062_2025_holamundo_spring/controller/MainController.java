40c1c41e00b9fd5156be755ce22d389b05680e42
elainehello <195331169+elainehello@users.noreply.github.com>
2025-05-09 18:29:30 +0200

    first commit

--- src/main/java/com/getafesoftwarefactory/IFCT0062_2025_holamundo_spring/controller/MainController.java
+++ src/main/java/com/getafesoftwarefactory/IFCT0062_2025_holamundo_spring/controller/MainController.java
@@ -1,11 +1,15 @@
 package com.getafesoftwarefactory.IFCT0062_2025_holamundo_spring.controller;
 
+import com.getafesoftwarefactory.IFCT0062_2025_holamundo_spring.services.SaludadorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // This annotation determines or finds which is in charge to solve a specific request
 public class MainController {
 
+    @Autowired // 
+    SaludadorService saludadorService;
     @GetMapping("/")
     public String crazyName() {
         return "hello"; // return value determines the output result for the server
--- src/main/java/com/getafesoftwarefactory/IFCT0062_2025_holamundo_spring/services/SaludadorService.java
+++ src/main/java/com/getafesoftwarefactory/IFCT0062_2025_holamundo_spring/services/SaludadorService.java
@@ -1,4 +1,11 @@
 package com.getafesoftwarefactory.IFCT0062_2025_holamundo_spring.services;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class SaludadorService {
+    public String getSaludo(String name) {
+        return "Hola " + name;
+    }
 }
